e74a9205fbfc0fb91e633dffe415fa5e16455cad
Abubakr <thefox5000@gmail.com>
2023-10-26 16:21:33 +0300

    Lifting State Up

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,19 @@ import "./App.css";
 import Header from "./components/Header";
 import Card from "./components/Card";
 import Events from "./components/Events";
-import State1 from "./components/useState"
-import TextInputWithFocusButton from "./components/useRef"
+import State1 from "./components/useState";
+import TextInputWithFocusButton from "./components/useRef";
+import Fruits from "./components/LiftigState/Fruits";
+import FruitsCounter from "./components/LiftigState/FruitsCounter";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [fruits] = useState([
+    { fruitName: "apple", id: 1 },
+    { fruitName: "apple", id: 2 },
+    { fruitName: "plum", id: 3 },
+    { fruitName: "plum", id: 4 },
+    { fruitName: "plum", id: 5 }
+  ]);
 
   return (
     <>
@@ -17,11 +25,16 @@ function App() {
       {/* <Card h2={"Hello, This is an H2"} h3={"Hello, This is an H3"} />
       <Card h2={"Hello, This is an H2"} h3={"Hello, This is an H3"} />
       <Card h2={"Hello, This is an H2"} h3={"Hello, This is an H3"} /> */}
-      
-      
-      <Events />
-      <State1 />
-      <TextInputWithFocusButton />
+
+      {/* <Events /> */}
+      {/* <State1 /> */}
+      {/* <TextInputWithFocusButton /> */}
+
+      <div className="App">
+        <h1>Where should the state go?</h1>
+        <Fruits fruits={fruits} />
+        <FruitsCounter fruits={fruits} />
+      </div>
     </>
   );
 }
